d8de8359503fc0cc42a62c8435b7d294d0ffdac2
Ethan Hocking <eshocking253@gmail.com>
2020-03-27 10:50:11 -0600

    Add spec for user story 4

--- spec/professors/professors_show_spec.rb
+++ spec/professors/professors_show_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 
 RSpec.describe "As a visitor", type: :feature do
-  context "When I visit /professors" do
-    it "I see a list of professors with their information" do
+  context "When I visit /professors/:id" do
+    it "I see a list of that professor's students" do
 
       snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
       hagrid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
@@ -26,6 +26,28 @@ RSpec.describe "As a visitor", type: :feature do
       expect(page).to have_content("Neville Longbottom")
 
 
+    end
+
+    it "I can see the average age of all that professor's students" do
+
+      snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+      hagrid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+      lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+      harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor")
+      malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin")
+      longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+
+      ProfessorStudent.create(student: harry, professor: snape)
+      ProfessorStudent.create(student: harry, professor: hagrid)
+      ProfessorStudent.create(student: harry, professor: lupin)
+      ProfessorStudent.create(student: malfoy, professor: hagrid)
+      ProfessorStudent.create(student: malfoy, professor: lupin)
+      ProfessorStudent.create(student: longbottom, professor: snape)
+
+      visit "/professors/#{hagrid.id}"
+
+      expect(page).to have_content("Average age: #{(harry.age.to_f + malfoy.age.to_f)/2}")
     end
   end
 end
