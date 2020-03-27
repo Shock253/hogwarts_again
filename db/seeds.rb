981d0d537aa546feed696552a0566d36664a6672
Ethan Hocking <eshocking253@gmail.com>
2020-03-27 09:20:18 -0600

    Fix your blatant lies in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 
 snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
-hagarid = Professor.create(name: "Rubus Hagarid", age: 38 , specialty: "Care of Magical Creatures")
+hagrid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
 lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
 
 harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
@@ -16,8 +16,8 @@ malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
 longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
 
 ProfessorStudent.create(student_id: harry, professor_id: snape)
-ProfessorStudent.create(student_id: harry, professor_id: hagarid)
+ProfessorStudent.create(student_id: harry, professor_id: hagrid)
 ProfessorStudent.create(student_id: harry, professor_id: lupin)
-ProfessorStudent.create(student_id: malfoy, professor_id: hagarid)
+ProfessorStudent.create(student_id: malfoy, professor_id: hagrid)
 ProfessorStudent.create(student_id: malfoy, professor_id: lupin)
 ProfessorStudent.create(student_id: longbottom, professor_id: snape)
